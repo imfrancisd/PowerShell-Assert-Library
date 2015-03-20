db0c243d955d7d5279d3c5e3ed2e2b155bc9859a
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2015-03-20 16:03:20 -0500

    Fix test for Assert-True.
    Test error records instead of error messages.
    Test that Assert-True never outputs anything.

--- test/Layer1/Test-Assert-True.ps1
+++ test/Layer1/Test-Assert-True.ps1
@@ -22,12 +22,18 @@ $nonBooleanTrue = @(
 & {
     Write-Verbose -Message 'Test Assert-True with Boolean $true' -Verbose:$headerVerbosity
 
-    $e1 = try {Assert-True $true | Out-Null} catch {$_.Exception}
-    $e2 = try {Assert-True -Value $true | Out-Null} catch {$_.Exception}
+    $out1 = $null
+    $out2 = $null
+    $er1 = try {Assert-True $true -OutVariable out1 | Out-Null} catch {$_}
+    $er2 = try {Assert-True -Value $true -OutVariable out2 | Out-Null} catch {$_}
 
-    $fail = ($null -ne $e1) -or ($null -ne $e2)
+    $pass =
+        ($out1.Count -eq 0) -and
+        ($out2.Count -eq 0) -and
+        ($null -eq $er1) -and
+        ($null -eq $er2)
 
-    if ($fail) {
+    if (-not $pass) {
         $message = 'Assert-True failed with $true value.'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
@@ -36,16 +42,20 @@ $nonBooleanTrue = @(
 & {
     Write-Verbose -Message 'Test Assert-True with Boolean $false' -Verbose:$headerVerbosity
 
-    $e1 = try {Assert-True $false | Out-Null} catch {$_.Exception}
-    $e2 = try {Assert-True -Value $false | Out-Null} catch {$_.Exception}
+    $out1 = $null
+    $out2 = $null
+    $er1 = try {Assert-True $false -OutVariable out1 | Out-Null} catch {$_}
+    $er2 = try {Assert-True -Value $false -OutVariable out2 | Out-Null} catch {$_}
 
-    $fail =
-        ($null -eq $e1) -or
-        ($null -eq $e2) -or
-        (-not $e1.Message.StartsWith('Assertion failed:', [System.StringComparison]::OrdinalIgnoreCase)) -or
-        (-not $e2.Message.StartsWith('Assertion failed:', [System.StringComparison]::OrdinalIgnoreCase))
+    $pass =
+        ($out1.Count -eq 0) -and
+        ($out2.Count -eq 0) -and
+        ($er1 -is [System.Management.Automation.ErrorRecord]) -and
+        ($er1.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-True', [System.StringComparison]::OrdinalIgnoreCase)) -and
+        ($er2 -is [System.Management.Automation.ErrorRecord]) -and
+        ($er2.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-True', [System.StringComparison]::OrdinalIgnoreCase))
 
-    if ($fail) {
+    if (-not $pass) {
         $message = 'Assert-True failed with $false value.'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
@@ -54,16 +64,20 @@ $nonBooleanTrue = @(
 & {
     Write-Verbose -Message 'Test Assert-True with $null' -Verbose:$headerVerbosity
 
-    $e1 = try {Assert-True $null | Out-Null} catch {$_.Exception}
-    $e2 = try {Assert-True -Value $null | Out-Null} catch {$_.Exception}
+    $out1 = $null
+    $out2 = $null
+    $er1 = try {Assert-True $null -OutVariable out1 | Out-Null} catch {$_}
+    $er2 = try {Assert-True -Value $null -OutVariable out2 | Out-Null} catch {$_}
 
-    $fail =
-        ($null -eq $e1) -or
-        ($null -eq $e2) -or
-        (-not $e1.Message.StartsWith('Assertion failed:', [System.StringComparison]::OrdinalIgnoreCase)) -or
-        (-not $e2.Message.StartsWith('Assertion failed:', [System.StringComparison]::OrdinalIgnoreCase))
+    $pass =
+        ($out1.Count -eq 0) -and
+        ($out2.Count -eq 0) -and
+        ($er1 -is [System.Management.Automation.ErrorRecord]) -and
+        ($er1.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-True', [System.StringComparison]::OrdinalIgnoreCase)) -and
+        ($er2 -is [System.Management.Automation.ErrorRecord]) -and
+        ($er2.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-True', [System.StringComparison]::OrdinalIgnoreCase))
 
-    if ($fail) {
+    if (-not $pass) {
         $message = 'Assert-True failed with $null value.'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
@@ -73,16 +87,20 @@ $nonBooleanTrue = @(
     Write-Verbose -Message 'Test Assert-True with Non-Booleans that are convertible to $true' -Verbose:$headerVerbosity
 
     foreach ($item in $nonBooleanTrue) {
-        $e1 = try {Assert-True $item | Out-Null} catch {$_.Exception}
-        $e2 = try {Assert-True -Value $item | Out-Null} catch {$_.Exception}
+        $out1 = $null
+        $out2 = $null
+        $er1 = try {Assert-True $item -OutVariable out1 | Out-Null} catch {$_}
+        $er2 = try {Assert-True -Value $item -OutVariable out2 | Out-Null} catch {$_}
 
-        $fail =
-            ($null -eq $e1) -or
-            ($null -eq $e2) -or
-            (-not $e1.Message.StartsWith('Assertion failed:', [System.StringComparison]::OrdinalIgnoreCase)) -or
-            (-not $e2.Message.StartsWith('Assertion failed:', [System.StringComparison]::OrdinalIgnoreCase))
+        $pass =
+            ($out1.Count -eq 0) -and
+            ($out2.Count -eq 0) -and
+            ($er1 -is [System.Management.Automation.ErrorRecord]) -and
+            ($er1.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-True', [System.StringComparison]::OrdinalIgnoreCase)) -and
+            ($er2 -is [System.Management.Automation.ErrorRecord]) -and
+            ($er2.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-True', [System.StringComparison]::OrdinalIgnoreCase))
 
-        if ($fail) {
+        if (-not $pass) {
             $message = 'Assert-True failed with Non-Boolean that is convertible to $true: ' + "$item"
             throw New-Object 'System.Exception' -ArgumentList @($message)
         }
@@ -93,16 +111,20 @@ $nonBooleanTrue = @(
     Write-Verbose -Message 'Test Assert-True with Non-Booleans that are convertible to $false' -Verbose:$headerVerbosity
 
     foreach ($item in $nonBooleanFalse) {
-        $e1 = try {Assert-True $item | Out-Null} catch {$_.Exception}
-        $e2 = try {Assert-True -Value $item | Out-Null} catch {$_.Exception}
+        $out1 = $null
+        $out2 = $null
+        $er1 = try {Assert-True $item -OutVariable out1 | Out-Null} catch {$_}
+        $er2 = try {Assert-True -Value $item -OutVariable out2 | Out-Null} catch {$_}
 
-        $fail =
-            ($null -eq $e1) -or
-            ($null -eq $e2) -or
-            (-not $e1.Message.StartsWith('Assertion failed:', [System.StringComparison]::OrdinalIgnoreCase)) -or
-            (-not $e2.Message.StartsWith('Assertion failed:', [System.StringComparison]::OrdinalIgnoreCase))
+        $pass =
+            ($out1.Count -eq 0) -and
+            ($out2.Count -eq 0) -and
+            ($er1 -is [System.Management.Automation.ErrorRecord]) -and
+            ($er1.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-True', [System.StringComparison]::OrdinalIgnoreCase)) -and
+            ($er2 -is [System.Management.Automation.ErrorRecord]) -and
+            ($er2.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-True', [System.StringComparison]::OrdinalIgnoreCase))
 
-        if ($fail) {
+        if (-not $pass) {
             $message = 'Assert-True failed with Non-Boolean that is convertible to $false: ' + "$item"
             throw New-Object 'System.Exception' -ArgumentList @($message)
         }
