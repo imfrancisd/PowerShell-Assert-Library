424dc2a2e4716a796580e0f48cfa3d6d40a593ac
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2015-03-22 20:01:40 -0500

    Add tests for Group-ListItem -Combine.
    Test Group-ListItem -Combine with the following values:
        * list of length 2.
    
    The tests for Group-ListItem -Combine can only use functions in the
    library that are at a lower level than Group-ListItem -Combine in the test
    hierarchy:
        * Assert-True
        * Assert-False
        * Assert-Null
        * Assert-NotNull
        * Assert-PipelineEmpty
        * Assert-PipelineAny
        * Assert-PipelineSingle
        * Assert-PipelineCount.

--- test/Layer3/Test-Group-ListItem.ps1
+++ test/Layer3/Test-Group-ListItem.ps1
@@ -717,6 +717,66 @@ if ($Silent) {
 & {
     Write-Verbose -Message 'Test Group-ListItem -Combine with lists of length 2' -Verbose:$headerVerbosity
 
+    $noarg = New-Object 'System.Object'
+
+    $list1 = @($null, 5)
+    $list2 = (New-Object -TypeName 'System.Collections.ArrayList' -ArgumentList @(,@('hello', 'world')))
+    $list3 = (New-Object -TypeName 'System.Collections.Generic.List[System.Double]' -ArgumentList @(,[System.Double[]]@(3.14, 2.72)))
+
+    function oracle($list, $size = 2)
+    {
+        switch ($size) {
+            0       {return @{'Items' = @()}}
+            1       {return @{'Items' = @(,$list[0])}, @{'Items' = @(,$list[1])}}
+            2       {return @{'Items' = @($list[0], $list[1])}}
+            default {return}
+        }
+    }
+
+    foreach ($size in @(-1, 0, 1, 2, 3, $noarg)) {
+        if ($noarg.Equals($size)) {
+            $gliArgs = @{}
+            $expectedSize = 2
+        } else {
+            $gliArgs = @{'Size' = $size}
+            $expectedSize = $size
+        }
+
+        $expected1 = @(oracle @gliArgs -list $list1)
+        $expected2 = @(oracle @gliArgs -list $list2)
+        $expected3 = @(oracle @gliArgs -list $list3)
+
+        $outputCount = $expected1.Length
+        Assert-True ($outputCount -eq $expected2.Length)
+        Assert-True ($outputCount -eq $expected3.Length)
+
+        $out1 = @(Group-ListItem @gliArgs -Combine $list1 | Assert-PipelineCount $outputCount | ForEach-Object {
+            Assert-True ($_ -isnot [System.Collections.IEnumerable])
+            Assert-True ($_.Items -is [System.Object[]])
+            Assert-True ($_.Items.Length -eq $expectedSize)
+            $_
+        })
+        $out2 = @(Group-ListItem @gliArgs -Combine $list2 | Assert-PipelineCount $outputCount | ForEach-Object {
+            Assert-True ($_ -isnot [System.Collections.IEnumerable])
+            Assert-True ($_.Items -is [System.Object[]])
+            Assert-True ($_.Items.Length -eq $expectedSize)
+            $_
+        })
+        $out3 = @(Group-ListItem @gliArgs -Combine $list3 | Assert-PipelineCount $outputCount | ForEach-Object {
+            Assert-True ($_ -isnot [System.Collections.IEnumerable])
+            Assert-True ($_.Items -is [System.Double[]])
+            Assert-True ($_.Items.Length -eq $expectedSize)
+            $_
+        })
+
+        for ($i = 0; $i -lt $outputCount; $i++) {
+            for ($j = 0; $j -lt $expectedSize; $j++) {
+                Assert-True ($expected1[$i].Items[$j] -eq $out1[$i].Items[$j])
+                Assert-True ($expected2[$i].Items[$j] -eq $out2[$i].Items[$j])
+                Assert-True ($expected3[$i].Items[$j] -eq $out3[$i].Items[$j])
+            }
+        }
+    }
     Write-Warning -Message 'Not implemented here.' -WarningAction 'Continue'
 }
 
