28ede1cdc7523618fb92da4edd4cbf6162978041
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2015-04-09 02:54:06 -0500

    Add tests for Test-Number.
    Add parameter tests for Test-Number.

--- test/Layer4/Test-Test-Number.ps1
+++ test/Layer4/Test-Test-Number.ps1
@@ -28,6 +28,365 @@ if ($Silent) {
 }
 
 & {
+    Write-Verbose -Message 'Test Test-Number ParameterSet: IsNumber' -Verbose:$headerVerbosity
+
+    $paramSet = (Get-Command -Name Test-Number).ParameterSets |
+        Where-Object {'IsNumber'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $valueParam = $paramSet.Parameters |
+        Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $typeParam = $paramSet.Parameters |
+        Where-Object {'Type'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $isParam = $paramSet.Parameters |
+        Where-Object {'IsNumber'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    Assert-True ($valueParam.IsMandatory)
+    Assert-False ($valueParam.ValueFromPipeline)
+    Assert-False ($valueParam.ValueFromPipelineByPropertyName)
+    Assert-False ($valueParam.ValueFromRemainingArguments)
+    Assert-True (0 -eq $valueParam.Position)
+    Assert-True (0 -eq $valueParam.Aliases.Count)
+
+    Assert-False ($typeParam.IsMandatory)
+    Assert-False ($typeParam.ValueFromPipeline)
+    Assert-False ($typeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($typeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $typeParam.Position)
+    Assert-True (0 -eq $typeParam.Aliases.Count)
+
+    Assert-False ($isParam.IsMandatory)
+    Assert-False ($isParam.ValueFromPipeline)
+    Assert-False ($isParam.ValueFromPipelineByPropertyName)
+    Assert-False ($isParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $isParam.Position)
+    Assert-True (0 -eq $isParam.Aliases.Count)
+}
+
+& {
+    Write-Verbose -Message 'Test Test-Number ParameterSet: OpEquals' -Verbose:$headerVerbosity
+
+    $paramSet = (Get-Command -Name Test-Number).ParameterSets |
+        Where-Object {'OpEquals'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $valueParam = $paramSet.Parameters |
+        Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $typeParam = $paramSet.Parameters |
+        Where-Object {'Type'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $matchTypeParam = $paramSet.Parameters |
+        Where-Object {'MatchType'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $eqParam = $paramSet.Parameters |
+        Where-Object {'Equals'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    Assert-True ($valueParam.IsMandatory)
+    Assert-False ($valueParam.ValueFromPipeline)
+    Assert-False ($valueParam.ValueFromPipelineByPropertyName)
+    Assert-False ($valueParam.ValueFromRemainingArguments)
+    Assert-True (0 -eq $valueParam.Position)
+    Assert-True (0 -eq $valueParam.Aliases.Count)
+
+    Assert-False ($typeParam.IsMandatory)
+    Assert-False ($typeParam.ValueFromPipeline)
+    Assert-False ($typeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($typeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $typeParam.Position)
+    Assert-True (0 -eq $typeParam.Aliases.Count)
+
+    Assert-False ($matchTypeParam.IsMandatory)
+    Assert-False ($matchTypeParam.ValueFromPipeline)
+    Assert-False ($matchTypeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($matchTypeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $matchTypeParam.Position)
+    Assert-True (0 -eq $matchTypeParam.Aliases.Count)
+
+    Assert-True ($eqParam.IsMandatory)
+    Assert-False ($eqParam.ValueFromPipeline)
+    Assert-False ($eqParam.ValueFromPipelineByPropertyName)
+    Assert-False ($eqParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $eqParam.Position)
+    Assert-True (1 -eq $eqParam.Aliases.Count)
+    Assert-True ('eq'.Equals($eqParam.Aliases[0], [System.StringComparison]::OrdinalIgnoreCase))
+}
+
+& {
+    Write-Verbose -Message 'Test Test-Number ParameterSet: OpNotEquals' -Verbose:$headerVerbosity
+
+    $paramSet = (Get-Command -Name Test-Number).ParameterSets |
+        Where-Object {'OpNotEquals'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $valueParam = $paramSet.Parameters |
+        Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $typeParam = $paramSet.Parameters |
+        Where-Object {'Type'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $matchTypeParam = $paramSet.Parameters |
+        Where-Object {'MatchType'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $neParam = $paramSet.Parameters |
+        Where-Object {'NotEquals'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    Assert-True ($valueParam.IsMandatory)
+    Assert-False ($valueParam.ValueFromPipeline)
+    Assert-False ($valueParam.ValueFromPipelineByPropertyName)
+    Assert-False ($valueParam.ValueFromRemainingArguments)
+    Assert-True (0 -eq $valueParam.Position)
+    Assert-True (0 -eq $valueParam.Aliases.Count)
+
+    Assert-False ($typeParam.IsMandatory)
+    Assert-False ($typeParam.ValueFromPipeline)
+    Assert-False ($typeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($typeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $typeParam.Position)
+    Assert-True (0 -eq $typeParam.Aliases.Count)
+
+    Assert-False ($matchTypeParam.IsMandatory)
+    Assert-False ($matchTypeParam.ValueFromPipeline)
+    Assert-False ($matchTypeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($matchTypeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $matchTypeParam.Position)
+    Assert-True (0 -eq $matchTypeParam.Aliases.Count)
+
+    Assert-True ($neParam.IsMandatory)
+    Assert-False ($neParam.ValueFromPipeline)
+    Assert-False ($neParam.ValueFromPipelineByPropertyName)
+    Assert-False ($neParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $neParam.Position)
+    Assert-True (1 -eq $neParam.Aliases.Count)
+    Assert-True ('ne'.Equals($neParam.Aliases[0], [System.StringComparison]::OrdinalIgnoreCase))
+}
+
+& {
+    Write-Verbose -Message 'Test Test-Number ParameterSet: OpLessThan' -Verbose:$headerVerbosity
+
+    $paramSet = (Get-Command -Name Test-Number).ParameterSets |
+        Where-Object {'OpLessThan'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $valueParam = $paramSet.Parameters |
+        Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $typeParam = $paramSet.Parameters |
+        Where-Object {'Type'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $matchTypeParam = $paramSet.Parameters |
+        Where-Object {'MatchType'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $ltParam = $paramSet.Parameters |
+        Where-Object {'LessThan'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    Assert-True ($valueParam.IsMandatory)
+    Assert-False ($valueParam.ValueFromPipeline)
+    Assert-False ($valueParam.ValueFromPipelineByPropertyName)
+    Assert-False ($valueParam.ValueFromRemainingArguments)
+    Assert-True (0 -eq $valueParam.Position)
+    Assert-True (0 -eq $valueParam.Aliases.Count)
+
+    Assert-False ($typeParam.IsMandatory)
+    Assert-False ($typeParam.ValueFromPipeline)
+    Assert-False ($typeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($typeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $typeParam.Position)
+    Assert-True (0 -eq $typeParam.Aliases.Count)
+
+    Assert-False ($matchTypeParam.IsMandatory)
+    Assert-False ($matchTypeParam.ValueFromPipeline)
+    Assert-False ($matchTypeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($matchTypeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $matchTypeParam.Position)
+    Assert-True (0 -eq $matchTypeParam.Aliases.Count)
+
+    Assert-True ($ltParam.IsMandatory)
+    Assert-False ($ltParam.ValueFromPipeline)
+    Assert-False ($ltParam.ValueFromPipelineByPropertyName)
+    Assert-False ($ltParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $ltParam.Position)
+    Assert-True (1 -eq $ltParam.Aliases.Count)
+    Assert-True ('lt'.Equals($ltParam.Aliases[0], [System.StringComparison]::OrdinalIgnoreCase))
+}
+
+& {
+    Write-Verbose -Message 'Test Test-Number ParameterSet: OpLessThanOrEqualTo' -Verbose:$headerVerbosity
+
+    $paramSet = (Get-Command -Name Test-Number).ParameterSets |
+        Where-Object {'OpLessThanOrEqualTo'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $valueParam = $paramSet.Parameters |
+        Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $typeParam = $paramSet.Parameters |
+        Where-Object {'Type'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $matchTypeParam = $paramSet.Parameters |
+        Where-Object {'MatchType'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $leParam = $paramSet.Parameters |
+        Where-Object {'LessThanOrEqualTo'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    Assert-True ($valueParam.IsMandatory)
+    Assert-False ($valueParam.ValueFromPipeline)
+    Assert-False ($valueParam.ValueFromPipelineByPropertyName)
+    Assert-False ($valueParam.ValueFromRemainingArguments)
+    Assert-True (0 -eq $valueParam.Position)
+    Assert-True (0 -eq $valueParam.Aliases.Count)
+
+    Assert-False ($typeParam.IsMandatory)
+    Assert-False ($typeParam.ValueFromPipeline)
+    Assert-False ($typeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($typeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $typeParam.Position)
+    Assert-True (0 -eq $typeParam.Aliases.Count)
+
+    Assert-False ($matchTypeParam.IsMandatory)
+    Assert-False ($matchTypeParam.ValueFromPipeline)
+    Assert-False ($matchTypeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($matchTypeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $matchTypeParam.Position)
+    Assert-True (0 -eq $matchTypeParam.Aliases.Count)
+
+    Assert-True ($leParam.IsMandatory)
+    Assert-False ($leParam.ValueFromPipeline)
+    Assert-False ($leParam.ValueFromPipelineByPropertyName)
+    Assert-False ($leParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $leParam.Position)
+    Assert-True (1 -eq $leParam.Aliases.Count)
+    Assert-True ('le'.Equals($leParam.Aliases[0], [System.StringComparison]::OrdinalIgnoreCase))
+}
+
+& {
+    Write-Verbose -Message 'Test Test-Number ParameterSet: OpGreaterThan' -Verbose:$headerVerbosity
+
+    $paramSet = (Get-Command -Name Test-Number).ParameterSets |
+        Where-Object {'OpGreaterThan'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $valueParam = $paramSet.Parameters |
+        Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $typeParam = $paramSet.Parameters |
+        Where-Object {'Type'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $matchTypeParam = $paramSet.Parameters |
+        Where-Object {'MatchType'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $gtParam = $paramSet.Parameters |
+        Where-Object {'GreaterThan'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    Assert-True ($valueParam.IsMandatory)
+    Assert-False ($valueParam.ValueFromPipeline)
+    Assert-False ($valueParam.ValueFromPipelineByPropertyName)
+    Assert-False ($valueParam.ValueFromRemainingArguments)
+    Assert-True (0 -eq $valueParam.Position)
+    Assert-True (0 -eq $valueParam.Aliases.Count)
+
+    Assert-False ($typeParam.IsMandatory)
+    Assert-False ($typeParam.ValueFromPipeline)
+    Assert-False ($typeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($typeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $typeParam.Position)
+    Assert-True (0 -eq $typeParam.Aliases.Count)
+
+    Assert-False ($matchTypeParam.IsMandatory)
+    Assert-False ($matchTypeParam.ValueFromPipeline)
+    Assert-False ($matchTypeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($matchTypeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $matchTypeParam.Position)
+    Assert-True (0 -eq $matchTypeParam.Aliases.Count)
+
+    Assert-True ($gtParam.IsMandatory)
+    Assert-False ($gtParam.ValueFromPipeline)
+    Assert-False ($gtParam.ValueFromPipelineByPropertyName)
+    Assert-False ($gtParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $gtParam.Position)
+    Assert-True (1 -eq $gtParam.Aliases.Count)
+    Assert-True ('gt'.Equals($gtParam.Aliases[0], [System.StringComparison]::OrdinalIgnoreCase))
+}
+
+& {
+    Write-Verbose -Message 'Test Test-Number ParameterSet: OpGreaterThanOrEqualTo' -Verbose:$headerVerbosity
+
+    $paramSet = (Get-Command -Name Test-Number).ParameterSets |
+        Where-Object {'OpGreaterThanOrEqualTo'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $valueParam = $paramSet.Parameters |
+        Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $typeParam = $paramSet.Parameters |
+        Where-Object {'Type'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $matchTypeParam = $paramSet.Parameters |
+        Where-Object {'MatchType'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    $geParam = $paramSet.Parameters |
+        Where-Object {'GreaterThanOrEqualTo'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)} |
+        Assert-PipelineSingle
+
+    Assert-True ($valueParam.IsMandatory)
+    Assert-False ($valueParam.ValueFromPipeline)
+    Assert-False ($valueParam.ValueFromPipelineByPropertyName)
+    Assert-False ($valueParam.ValueFromRemainingArguments)
+    Assert-True (0 -eq $valueParam.Position)
+    Assert-True (0 -eq $valueParam.Aliases.Count)
+
+    Assert-False ($typeParam.IsMandatory)
+    Assert-False ($typeParam.ValueFromPipeline)
+    Assert-False ($typeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($typeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $typeParam.Position)
+    Assert-True (0 -eq $typeParam.Aliases.Count)
+
+    Assert-False ($matchTypeParam.IsMandatory)
+    Assert-False ($matchTypeParam.ValueFromPipeline)
+    Assert-False ($matchTypeParam.ValueFromPipelineByPropertyName)
+    Assert-False ($matchTypeParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $matchTypeParam.Position)
+    Assert-True (0 -eq $matchTypeParam.Aliases.Count)
+
+    Assert-True ($geParam.IsMandatory)
+    Assert-False ($geParam.ValueFromPipeline)
+    Assert-False ($geParam.ValueFromPipelineByPropertyName)
+    Assert-False ($geParam.ValueFromRemainingArguments)
+    Assert-True (0 -gt $geParam.Position)
+    Assert-True (1 -eq $geParam.Aliases.Count)
+    Assert-True ('ge'.Equals($geParam.Aliases[0], [System.StringComparison]::OrdinalIgnoreCase))
+}
+
+& {
     Write-Verbose -Message 'Test Test-Number -IsNumber' -Verbose:$headerVerbosity
 
     $numbers = [System.Object[]]@(0, [System.Single]1.0, 2.718, [System.Decimal]3.1416)
@@ -38,11 +397,6 @@ if ($Silent) {
         Assert-True  (Test-Number $number -IsNumber)
         Assert-True  (Test-Number $number -IsNumber:$true)
         Assert-False (Test-Number $number -IsNumber:$false)
-
-        Assert-True  (Test-Number -Value $number)
-        Assert-True  (Test-Number -Value $number -IsNumber)
-        Assert-True  (Test-Number -Value $number -IsNumber:$true)
-        Assert-False (Test-Number -Value $number -IsNumber:$false)
     }
 
     foreach ($nonNumber in $nonNumbers) {
@@ -50,11 +404,6 @@ if ($Silent) {
         Assert-False (Test-Number $nonNumber -IsNumber)
         Assert-False (Test-Number $nonNumber -IsNumber:$true)
         Assert-True  (Test-Number $nonNumber -IsNumber:$false)
-
-        Assert-False (Test-Number -Value $nonNumber)
-        Assert-False (Test-Number -Value $nonNumber -IsNumber)
-        Assert-False (Test-Number -Value $nonNumber -IsNumber:$true)
-        Assert-True  (Test-Number -Value $nonNumber -IsNumber:$false)
     }
 }
 
