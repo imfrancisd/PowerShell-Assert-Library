4920978f9541e980d9517ee3d27435018dfd4c2d
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2015-04-21 17:24:36 -0500

    Add initial versions of quantifiers.
    Add initial versions of Assert-All, Assert-Exists, Assert-NotExists.
    
    Assert-NotAll was not added because the meaning of "not all" is confusing
    when the collection is empty.

---
+++ help/Script/en-US/Assert-All.psd1
@@ -0,0 +1,59 @@
+<#
+.Synopsis
+Assert that a predicate is true for all items in a collection.
+.Description
+This function throws an error if any of the following conditions are met:
+    *the predicate is not true for at least one item in the collection
+
+Note:
+The assertion will always pass if the collection is empty.
+
+*See the -Collection and -Predicate parameters for more details.
+.Parameter private:Collection
+The collection of items used to test the predicate.
+
+The order in which the items in the collection are tested is determined by the collection's GetEnumerator method.
+.Parameter private:Predicate
+The script block that will be invoked on each item in the collection.
+
+The script block must take one argument and return a value.
+
+Note:
+The -ErrorAction parameter has NO effect on the predicate.
+An InvalidOperationException is thrown if the predicate throws an error.
+Set the $ErrorActionPreference variable inside the predicate if you need to use that variable.
+.Example
+.Example
+.Example
+.Inputs
+None
+
+This function does not accept input from the pipeline.
+.Outputs
+None
+.Notes
+.Link
+Assert-True
+.Link
+Assert-False
+.Link
+Assert-Null
+.Link
+Assert-NotTrue
+.Link
+Assert-NotFalse
+.Link
+Assert-NotNull
+.Link
+Assert-Exists
+.Link
+Assert-NotExists
+.Link
+Assert-PipelineEmpty
+.Link
+Assert-PipelineAny
+.Link
+Assert-PipelineSingle
+.Link
+Assert-PipelineCount
+#>
---
+++ src/AssertFunctions/Assert-Exists.ps1
@@ -0,0 +1,70 @@
+function Assert-Exists
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$true, ValueFromPipeline=$false, Position=0)]
+        [System.Collections.ICollection]
+        $private:Collection,
+
+        [Parameter(Mandatory=$true, ValueFromPipeline=$false, Position=1)]
+        [System.Management.Automation.ScriptBlock]
+        $private:Predicate
+    )
+
+    $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+    if (-not $PSBoundParameters.ContainsKey('Verbose')) {
+        $VerbosePreference = $PSCmdlet.GetVariableValue('VerbosePreference') -as [System.Management.Automation.ActionPreference]
+        if ($null -eq $VerbosePreference) {
+            $VerbosePreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+        }
+    }
+    if (-not $PSBoundParameters.ContainsKey('Debug')) {
+        $DebugPreference = $PSCmdlet.GetVariableValue('DebugPreference') -as [System.Management.Automation.ActionPreference]
+        if ($null -eq $DebugPreference) {
+            $DebugPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+        }
+    }
+
+    $private:fail = $true
+
+    foreach ($private:item in $private:Collection.psbase.GetEnumerator()) {
+        try   {$private:result = & $private:Predicate $private:item}
+        catch {
+            $private:errorRecord = New-Object -TypeName 'System.Management.Automation.ErrorRecord' -ArgumentList @(
+                (New-Object -TypeName 'System.InvalidOperationException' -ArgumentList @('Could not invoke predicate.', $_.Exception)),
+                'PredicateError',
+                [System.Management.Automation.ErrorCategory]::OperationStopped,
+                $private:Predicate
+            )
+            $PSCmdlet.ThrowTerminatingError($private:errorRecord)
+        }
+        
+        if (($private:result -is [System.Boolean]) -and $private:result) {
+            $private:fail = $false
+            break
+        }
+    }
+
+    if ($private:fail -or ($VerbosePreference -ne [System.Management.Automation.ActionPreference]::SilentlyContinue)) {
+        $private:message = 'Assertion {0}: {1}, file {2}, line {3}' -f @(
+            $(if ($fail) {'failed'} else {'passed'}),
+            $MyInvocation.Line.Trim(),
+            $MyInvocation.ScriptName,
+            $MyInvocation.ScriptLineNumber
+        )
+
+        Write-Verbose -Message $private:message
+
+        if ($private:fail) {
+            Write-Debug -Message $private:message
+
+            $private:errorRecord = New-Object -TypeName 'System.Management.Automation.ErrorRecord' -ArgumentList @(
+                (New-Object -TypeName 'System.Exception' -ArgumentList @($private:message)),
+                'AssertionFailed',
+                [System.Management.Automation.ErrorCategory]::OperationStopped,
+                $private:Collection
+            )
+            $PSCmdlet.ThrowTerminatingError($private:errorRecord)
+        }
+    }
+}
