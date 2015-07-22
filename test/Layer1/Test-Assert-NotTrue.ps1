cee2ca166286c5c86842703a3936b37c99318f33
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2015-07-22 04:16:32 -0500

    Structure logging of tests for basic assert functions.
    Make the structure of the test log entries for the logic functions
    consistent. This structure will then be used for all other tests.
    
    Each test log entry will have this structure:
        [pscustomobject]@{
            File = #string (full path)
            Test = #string (test description)
            Pass = #boolean or null (null is for tests that did not finish)
            Data = @{
                in  = #IDictionary or IList (for named params or arguments)
                out = #IList (output from -OutVariable)
                err = #ErrorRecord from command that takes in and creates out
            }
        }
    
    Also, each data set will have its own log entry. This will allow test
    runners (and their loggers) to have greater control over how much test
    data they want to keep. This will be very important for tests where many
    sets of data are generated automatically.
    
    commit 751bafca1c638cdadbbbd0cdf58296300b945be8
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Wed Jul 22 04:10:17 2015 -0500
    
        Structure logging of tests for Assert-NotNull.
    
    commit 721f52691fb14489f9372e304d9d4f6e14887ec6
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Wed Jul 22 04:01:20 2015 -0500
    
        Structure logging of tests for Assert-Null.
    
    commit 5ecee9c6838e57ceee6652a77772dfe96667cd79
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Wed Jul 22 03:56:18 2015 -0500
    
        Structure logging of tests for Assert-NotTrue.
    
    commit 23cc60d4c9963b659fe54891e7127675ba414a2a
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Wed Jul 22 03:47:44 2015 -0500
    
        Structure logging of tests for Assert-True.
    
    commit 2be4a11511e74683ccf6f4009e32413b07db5b8c
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Wed Jul 22 03:41:23 2015 -0500
    
        Structure logging of tests for Assert-NotFalse.
    
    commit 80ae3389975a20427c66dc88fade09584790b8ec
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Wed Jul 22 03:28:05 2015 -0500
    
        Structure logging of tests for Assert-False.

--- test/Layer1/Test-Assert-NotTrue.ps1
+++ test/Layer1/Test-Assert-NotTrue.ps1
@@ -1,8 +1,69 @@
+#requires -version 2
+
+<#
+.Synopsis
+Test the Assert-NotTrue cmdlet.
+.Description
+Test the Assert-NotTrue cmdlet.
+.Inputs
+None
+.Outputs
+None
+.Notes
+=====================================================================
+
+
+#Use an ArrayList as a logger.
+#The log entries will be in $simpleLogger.
+#
+$simpleLogger = new-object -typename system.collections.arraylist
+.\Test-Assert-NotTrue.ps1 -logger $simpleLogger
+
+
+=====================================================================
+
+
+#Use a custom object as a logger.
+#The log entries will be in $customLogger.Entries.
+#
+$customLogger = new-object -typename psobject -property @{
+    Entries = new-object -typename system.collections.arraylist
+} |
+add-member scriptmethod Add {
+    param($logEntry)
+
+    #Add entry.
+    $this.Entries.Add($logEntry) | out-null
+
+    #Limit the number of entries in the logger to 10.
+    if ($this.Entries.Count -gt 10) {
+        $this.Entries.RemoveAt(0)
+    }
+} -passthru
+.\Test-Assert-NotTrue.ps1 -logger $customLogger
+
+
+=====================================================================
+#>
 [CmdletBinding()]
 Param(
+    #A data structure with an "Add" method that will be used to log tests.
+    #
+    #The data structure can be a simple ArrayList or a complicated custom object.
+    #The advantage of using a custom object is that you have full control over the logging behavior, such as limiting the number of log entries.
+    #
+    #See the Notes section for examples.
+    $Logger = $null,
+
+    #Suppress all verbose messages.
+    #
+    #The default is to suppress some verbose messages.
+    #Use the -Verbose switch (and turn off this switch) to display all verbose messages.
     [System.Management.Automation.SwitchParameter]
     $Silent
 )
+
+
 
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 if ($Silent) {
@@ -12,6 +73,28 @@ if ($Silent) {
     $headerVerbosity = [System.Management.Automation.ActionPreference]::Continue
 }
 
+$TestScriptFilePath = $MyInvocation.MyCommand.Path
+function newTestLogEntry($testDescription)
+{
+    Write-Verbose -Message $testDescription -Verbose:$headerVerbosity
+    $logEntry = New-Object -TypeName 'System.Management.Automation.PSObject' -Property @{
+        File = $TestScriptFilePath
+        Test = $testDescription
+        Pass = $null
+        Data = @{
+            in  = $null #IDictionary (for named parameters) or IList (for arguments)
+            out = $null #IList (one item per output, generated from -OutVariable parameter)
+            err = $null #ErrorRecord from command that takes in and generates out
+        }
+    }
+    if ($null -ne $Logger) {
+        [System.Void]$Logger.Add($logEntry)
+    }
+    return $logEntry
+}
+
+
+
 $nonBooleanFalse = @(
     0, '', @($null), @(0), @(''), @($false), @(), @(,@())
 )
@@ -20,43 +103,55 @@ $nonBooleanTrue = @(
 )
 
 & {
-    Write-Verbose -Message 'Test Assert-NotTrue with get-help -full' -Verbose:$headerVerbosity
+    $test = newTestLogEntry 'Test Assert-NotTrue with get-help -full'
 
-    $err = try {$fullHelp = Get-Help Assert-NotTrue -Full} catch {$_}
+    $test.Data.in  = @{name = 'Assert-NotTrue'}
+    $test.Data.err = try {Get-Help -Name $test.Data.in.name -Full -OutVariable out | Out-Null} catch {$_}
+    $test.Data.out = $out
 
-    $pass =
-        ($null -eq $err) -and
-        ($fullHelp.Name -is [System.String]) -and
-        ($fullHelp.Name.Equals('Assert-NotTrue', [System.StringComparison]::OrdinalIgnoreCase)) -and
-        ($fullHelp.description -is [System.Collections.ICollection]) -and
-        ($fullHelp.description.Count -gt 0) -and
-        ($null -ne $fullHelp.examples) -and
-        (0 -lt @($fullHelp.examples.example).Count) -and
-        ('' -ne @($fullHelp.examples.example)[0].code)
+    $test.Pass =
+        (Test-Null $test.Data.err) -and
+        ($test.Data.out.Count -eq 1) -and
+        ($test.Data.out[0].Name -is [System.String]) -and
+        ($test.Data.out[0].Name.Equals('Assert-NotTrue', [System.StringComparison]::OrdinalIgnoreCase)) -and
+        ($test.Data.out[0].description -is [System.Collections.ICollection]) -and
+        ($test.Data.out[0].description.Count -gt 0) -and
+        ($null -ne $test.Data.out[0].examples) -and
+        (0 -lt @($test.Data.out[0].examples.example).Count) -and
+        ('' -ne @($test.Data.out[0].examples.example)[0].code)
 
-    if (-not $pass) {
+    if (-not $test.Pass) {
         $message = 'Assert-NotTrue failed with get-help -full.'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
 }
 
 & {
-    Write-Verbose -Message 'Test Assert-NotTrue parameters' -Verbose:$headerVerbosity
+    $test = newTestLogEntry 'Test Assert-NotTrue parameters'
 
-    $message = 'Assert-NotTrue failed parameter tests.'
+    $test.Data.in  = @{name = 'Assert-NotTrue'}
+    $test.Data.err = try {Get-Command -Name $test.Data.in.name -OutVariable out | Out-Null} catch {$_}
+    $test.Data.out = $out
 
-    $paramSets = @((Get-Command -Name Assert-NotTrue).ParameterSets)
+    if (Test-NotNull $test.Data.err) {
+        $message = 'Assert-NotTrue failed parameter tests (could not get command).'
+        throw New-Object 'System.Exception' -ArgumentList @($message)
+    }
+
+    $paramSets = @($test.Data.out[0].ParameterSets)
     if (1 -ne $paramSets.Count) {
+        $message = 'Assert-NotTrue failed parameter tests (more than one parameter set).'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
 
     $valueParam = $paramSets[0].Parameters |
         Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)}
-    if ($null -eq $valueParam) {
+    if (Test-Null $valueParam) {
+        $message = 'Assert-NotTrue failed parameter tests (no parameter with name "Value").'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
 
-    $pass =
+    $test.Pass =
         ($valueParam.IsMandatory) -and
         ($valueParam.ParameterType -eq [System.Object]) -and
         (-not $valueParam.ValueFromPipeline) -and
@@ -65,113 +160,112 @@ $nonBooleanTrue = @(
         (0 -eq $valueParam.Position) -and
         (0 -eq $valueParam.Aliases.Count)
 
-    if (-not $pass) {
+    if (-not $test.Pass) {
+        $message = 'Assert-NotTrue failed parameter tests ("Value" parameter attributes).'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
 }
 
 & {
-    Write-Verbose -Message 'Test Assert-NotTrue with Boolean $true' -Verbose:$headerVerbosity
+    $test = newTestLogEntry 'Test Assert-NotTrue with Boolean $true'
 
-    $out1 = New-Object -TypeName 'System.Collections.ArrayList'
-    $out2 = New-Object -TypeName 'System.Collections.ArrayList'
-    $er1 = try {Assert-NotTrue $true -OutVariable out1 | Out-Null} catch {$_}
-    $er2 = try {Assert-NotTrue -Value $true -OutVariable out2 | Out-Null} catch {$_}
+    $test.Data.in  = @{value = $true}
+    $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+    $test.Data.out = $out
 
-    $pass =
-        ($out1.Count -eq 0) -and
-        ($out2.Count -eq 0) -and
-        ($er1 -is [System.Management.Automation.ErrorRecord]) -and
-        ($er1.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-NotTrue', [System.StringComparison]::OrdinalIgnoreCase)) -and
-        ($er2 -is [System.Management.Automation.ErrorRecord]) -and
-        ($er2.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-NotTrue', [System.StringComparison]::OrdinalIgnoreCase))
+    $test.Pass =
+        ($test.Data.err -is [System.Management.Automation.ErrorRecord]) -and
+        ($test.Data.err.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-NotTrue', [System.StringComparison]::OrdinalIgnoreCase)) -and
+        ($test.Data.out.Count -eq 0)
 
-    if (-not $pass) {
+    if (-not $test.Pass) {
         $message = 'Assert-NotTrue failed with $true value.'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
 }
 
 & {
-    Write-Verbose -Message 'Test Assert-NotTrue with Boolean $false' -Verbose:$headerVerbosity
+    $test = newTestLogEntry 'Test Assert-NotTrue with Boolean $false'
 
-    $out1 = New-Object -TypeName 'System.Collections.ArrayList'
-    $out2 = New-Object -TypeName 'System.Collections.ArrayList'
-    $er1 = try {Assert-NotTrue $false -OutVariable out1 | Out-Null} catch {$_}
-    $er2 = try {Assert-NotTrue -Value $false -OutVariable out2 | Out-Null} catch {$_}
+    $test.Data.in  = @{value = $false}
+    $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+    $test.Data.out = $out
 
-    $pass =
-        ($out1.Count -eq 0) -and
-        ($out2.Count -eq 0) -and
-        ($null -eq $er1) -and
-        ($null -eq $er2)
+    $test.Pass =
+        (Test-Null $test.Data.err) -and
+        ($test.Data.out.Count -eq 0)
 
-    if (-not $pass) {
+    if (-not $test.Pass) {
         $message = 'Assert-NotTrue failed with $false value.'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
 }
 
 & {
-    Write-Verbose -Message 'Test Assert-NotTrue with $null' -Verbose:$headerVerbosity
+    $test = newTestLogEntry 'Test Assert-NotTrue with $null'
 
-    $out1 = New-Object -TypeName 'System.Collections.ArrayList'
-    $out2 = New-Object -TypeName 'System.Collections.ArrayList'
-    $er1 = try {Assert-NotTrue $null -OutVariable out1 | Out-Null} catch {$_}
-    $er2 = try {Assert-NotTrue -Value $null -OutVariable out2 | Out-Null} catch {$_}
+    $test.Data.in  = @{value = $null}
+    $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+    $test.Data.out = $out
 
-    $pass =
-        ($out1.Count -eq 0) -and
-        ($out2.Count -eq 0) -and
-        ($null -eq $er1) -and
-        ($null -eq $er2)
+    $test.Pass =
+        (Test-Null $test.Data.err) -and
+        ($test.Data.out.Count -eq 0)
 
-    if (-not $pass) {
+    if (-not $test.Pass) {
         $message = 'Assert-NotTrue failed with $null value.'
         throw New-Object 'System.Exception' -ArgumentList @($message)
     }
 }
 
 & {
-    Write-Verbose -Message 'Test Assert-NotTrue with Non-Booleans that are convertible to $true' -Verbose:$headerVerbosity
+    for ($i = 0; $i -lt $nonBooleanTrue.Count; $i++) {
+        $test = newTestLogEntry 'Test Assert-NotTrue with Non-Boolean that is convertible to $true'
 
-    foreach ($item in $nonBooleanTrue) {
-        $out1 = New-Object -TypeName 'System.Collections.ArrayList'
-        $out2 = New-Object -TypeName 'System.Collections.ArrayList'
-        $er1 = try {Assert-NotTrue $item -OutVariable out1 | Out-Null} catch {$_}
-        $er2 = try {Assert-NotTrue -Value $item -OutVariable out2 | Out-Null} catch {$_}
+        $test.Data.in  = @{value = $nonBooleanTrue[$i]}
+        $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+        $test.Data.out = $out
 
-        $pass =
-            ($out1.Count -eq 0) -and
-            ($out2.Count -eq 0) -and
-            ($null -eq $er1) -and
-            ($null -eq $er2)
+        $test.Pass =
+            (Test-Null $test.Data.err) -and
+            ($test.Data.out.Count -eq 0)
 
-        if (-not $pass) {
-            $message = 'Assert-NotTrue failed with Non-Boolean that is convertible to $true: ' + "$item"
+        if (-not $test.Pass) {
+            $message = 'Assert-NotTrue failed with Non-Boolean that is convertible to $true.'
             throw New-Object 'System.Exception' -ArgumentList @($message)
         }
+    }
+
+    if ($i -eq 0) {
+        $test = newTestLogEntry 'Test Assert-NotTrue with Non-Boolean that is convertible to $true'
+
+        $message = 'No data to test Assert-NotTrue with Non-Boolean that is convertible to $true.'
+        throw New-Object 'System.Exception' -ArgumentList @($message)
     }
 }
 
 & {
-    Write-Verbose -Message 'Test Assert-NotTrue with Non-Booleans that are convertible to $false' -Verbose:$headerVerbosity
+    for ($i = 0; $i -lt $nonBooleanFalse.Count; $i++) {
+        $test = newTestLogEntry 'Test Assert-NotTrue with Non-Boolean that is convertible to $false'
 
-    foreach ($item in $nonBooleanFalse) {
-        $out1 = New-Object -TypeName 'System.Collections.ArrayList'
-        $out2 = New-Object -TypeName 'System.Collections.ArrayList'
-        $er1 = try {Assert-NotTrue $item -OutVariable out1 | Out-Null} catch {$_}
-        $er2 = try {Assert-NotTrue -Value $item -OutVariable out2 | Out-Null} catch {$_}
+        $test.Data.in  = @{value = $nonBooleanFalse[$i]}
+        $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+        $test.Data.out = $out
 
-        $pass =
-            ($out1.Count -eq 0) -and
-            ($out2.Count -eq 0) -and
-            ($null -eq $er1) -and
-            ($null -eq $er2)
+        $test.Pass =
+            (Test-Null $test.Data.err) -and
+            ($test.Data.out.Count -eq 0)
 
-        if (-not $pass) {
-            $message = 'Assert-NotTrue failed with Non-Boolean that is convertible to $false: ' + "$item"
+        if (-not $test.Pass) {
+            $message = 'Assert-NotTrue failed with Non-Boolean that is convertible to $false.'
             throw New-Object 'System.Exception' -ArgumentList @($message)
         }
+    }
+
+    if ($i -eq 0) {
+        $test = newTestLogEntry 'Test Assert-NotTrue with Non-Boolean that is convertible to $false'
+
+        $message = 'No data to test Assert-NotTrue with Non-Boolean that is convertible to $false.'
+        throw New-Object 'System.Exception' -ArgumentList @($message)
     }
 }
