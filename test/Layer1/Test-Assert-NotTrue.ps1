856933a24a5ed1fd9ef9266f87c5546db4e01f97
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2015-07-25 04:58:38 -0500

    Modify logging of tests for basic assert functions.
    Update the structure of the log entries for the basic assert functions to
    match the newest version.
    
    commit d94439bd12940034d7c2698a99dd0da0f6b489d9
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Sat Jul 25 04:53:12 2015 -0500
    
        Modify logging of tests for Assert-NotNull.
    
    commit 74c004c8cc35bfce21a2d521b2e8f2a86f88ebf2
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Sat Jul 25 04:45:09 2015 -0500
    
        Modify logging of tests for Assert-Null.
    
    commit 8b83bd00833aba2dd515a4371f97b07721799281
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Sat Jul 25 04:38:07 2015 -0500
    
        Modify logging of tests for Assert-NotFalse.
    
    commit 1147939938afe9a05985290efb48d48f1dc44afd
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Sat Jul 25 04:30:48 2015 -0500
    
        Modify logging of tests for Assert-False.
    
    commit 3834fd03aa431e1b46825c2e66da524b60e3c98b
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Sat Jul 25 04:23:17 2015 -0500
    
        Modify logging of tests for Assert-NotTrue.
    
    commit e2ba67ab8f3c76b9b17051f56da1c8caa04de415
    Author: Francis de la Cerna <imfrancisd@users.noreply.github.com>
    Date:   Sat Jul 25 04:13:33 2015 -0500
    
        Modify logging of tests for Assert-True.

--- test/Layer1/Test-Assert-NotTrue.ps1
+++ test/Layer1/Test-Assert-NotTrue.ps1
@@ -44,15 +44,41 @@ add-member scriptmethod Add {
 
 
 =====================================================================
+
+
+Log Entry Structure
+===================
+All log entries will have the same structure.
+A log entry will not change after it is added to the logger.
+
+[pscustomobject]@{
+    File = #String (full path)
+    Test = #String (test description)
+    Pass = #Boolean (test result, $null if inconclusive)
+    Data = @{
+        err = #ErrorRecord from command that takes in and creates out
+        out = #IList (generated from -OutVariable parameter)
+        in  = #IDictionary (for parameter names/values
+              #list of args, if any, will be in entry with key '')
+    }
+    Time = @{
+        start = #DateTime UTC (test log entry creation time)
+        stop  = #DateTime UTC (test log entry log time)
+        span  = #TimeSpan (test duration)
+    }
+}
+
+
+=====================================================================
 #>
 [CmdletBinding()]
 Param(
     #A data structure with an "Add" method that will be used to log tests.
     #
     #The data structure can be a simple ArrayList or a complicated custom object.
-    #The advantage of using a custom object is that you have full control over the logging behavior, such as limiting the number of log entries.
+    #The advantage of using a custom object is that you have full control over the logging behavior such as limiting the number of log entries.
     #
-    #See the Notes section for examples.
+    #See the Notes section for more details and examples.
     $Logger = $null,
 
     #Suppress all verbose messages.
@@ -73,24 +99,56 @@ if ($Silent) {
     $headerVerbosity = [System.Management.Automation.ActionPreference]::Continue
 }
 
+
+
 $TestScriptFilePath = $MyInvocation.MyCommand.Path
-function newTestLogEntry($testDescription)
+$TestScriptStopWatch = New-Object -TypeName 'System.Diagnostics.Stopwatch'
+
+function newTestLogEntry
 {
-    Write-Verbose -Message $testDescription -Verbose:$headerVerbosity
+    param(
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $testDescription
+    )
+
+    if ([System.Int32]$headerVerbosity) {
+        $VerbosePreference = $headerVerbosity
+        Write-Verbose -Message $testDescription
+    }
+
     $logEntry = New-Object -TypeName 'System.Management.Automation.PSObject' -Property @{
         File = $TestScriptFilePath
         Test = $testDescription
         Pass = $null
-        Data = @{
-            in  = $null #IDictionary (for named parameters) or IList (for arguments)
-            out = $null #IList (one item per output, generated from -OutVariable parameter)
-            err = $null #ErrorRecord from command that takes in and generates out
-        }
+        Data = @{err = $null; out = $null; in = $null;}
+        Time = @{start = [System.DateTime]::UtcNow; stop = $null; span = $null;}
     }
+
+    $TestScriptStopWatch.Reset()
+    $TestScriptStopWatch.Start()
+    return $logEntry
+}
+
+function commitTestLogEntry
+{
+    param(
+        [parameter(Mandatory = $true)]
+        $logEntry,
+        
+        [parameter(Mandatory = $false)]
+        [System.Boolean]
+        $pass
+    )
+
+    $logEntry.Pass = if ($PSBoundParameters.ContainsKey('pass')) {$pass} else {$null}
+    $TestScriptStopWatch.Stop()
+    $logEntry.Time.span = $TestScriptStopWatch.Elapsed
+    $logEntry.Time.stop = [System.DateTime]::UtcNow
+
     if ($null -ne $Logger) {
         [System.Void]$Logger.Add($logEntry)
     }
-    return $logEntry
 }
 
 
@@ -102,170 +160,200 @@ $nonBooleanTrue = @(
     1, 'True', 'False', @(1), @('True'), @('False'), @($true), @(,@(1)), @(,@($true))
 )
 
+
+
 & {
-    $test = newTestLogEntry 'Test Assert-NotTrue with get-help -full'
+    $testDescription = 'Assert-NotTrue help'
 
-    $test.Data.in  = @{name = 'Assert-NotTrue'}
-    $test.Data.err = try {Get-Help -Name $test.Data.in.name -Full -OutVariable out | Out-Null} catch {$_}
-    $test.Data.out = $out
+    $test = newTestLogEntry $testDescription
+    $pass = $false
+    try {
+        $test.Data.out = $out = @()
+        $test.Data.in  = @{name = 'Assert-NotTrue'}
+        $test.Data.err = try {Get-Help -Name $test.Data.in.name -Full -OutVariable out | Out-Null} catch {$_}
+        $test.Data.out = $out
 
-    $test.Pass =
-        (Test-Null $test.Data.err) -and
-        ($test.Data.out.Count -eq 1) -and
-        ($test.Data.out[0].Name -is [System.String]) -and
-        ($test.Data.out[0].Name.Equals('Assert-NotTrue', [System.StringComparison]::OrdinalIgnoreCase)) -and
-        ($test.Data.out[0].description -is [System.Collections.ICollection]) -and
-        ($test.Data.out[0].description.Count -gt 0) -and
-        ($null -ne $test.Data.out[0].examples) -and
-        (0 -lt @($test.Data.out[0].examples.example).Count) -and
-        ('' -ne @($test.Data.out[0].examples.example)[0].code)
+        $pass =
+            (Test-Null $test.Data.err) -and
+            ($test.Data.out.Count -eq 1) -and
+            ($test.Data.out[0].Name -is [System.String]) -and
+            ($test.Data.out[0].Name.Equals('Assert-NotTrue', [System.StringComparison]::OrdinalIgnoreCase)) -and
+            ($test.Data.out[0].description -is [System.Collections.ICollection]) -and
+            ($test.Data.out[0].description.Count -gt 0) -and
+            (Test-NotNull $test.Data.out[0].examples) -and
+            (0 -lt @($test.Data.out[0].examples.example).Count) -and
+            ('' -ne @($test.Data.out[0].examples.example)[0].code)
 
-    if (-not $test.Pass) {
-        $message = 'Assert-NotTrue failed with get-help -full.'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
+        if (-not $pass) {
+            throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+        }
     }
+    finally {commitTestLogEntry $test $pass}
 }
 
 & {
-    $test = newTestLogEntry 'Test Assert-NotTrue parameters'
+    $testDescription = 'Assert-NotTrue parameters'
 
-    $test.Data.in  = @{name = 'Assert-NotTrue'}
-    $test.Data.err = try {Get-Command -Name $test.Data.in.name -OutVariable out | Out-Null} catch {$_}
-    $test.Data.out = $out
+    $test = newTestLogEntry $testDescription
+    $pass = $false
+    try {
+        $test.Data.out = $out = @()
+        $test.Data.in  = @{name = 'Assert-NotTrue'}
+        $test.Data.err = try {Get-Command -Name $test.Data.in.name -OutVariable out | Out-Null} catch {$_}
+        $test.Data.out = $out
 
-    if (Test-NotNull $test.Data.err) {
-        $message = 'Assert-NotTrue failed parameter tests (could not get command).'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
+        if (Test-NotNull $test.Data.err) {
+            throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+        }
+
+        $paramSets = @($test.Data.out[0].ParameterSets)
+        if (1 -ne $paramSets.Count) {
+            throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+        }
+
+        $valueParam = $paramSets[0].Parameters |
+            Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)}
+        if (Test-Null $valueParam) {
+            throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+        }
+
+        $pass =
+            ($valueParam.IsMandatory) -and
+            ($valueParam.ParameterType -eq [System.Object]) -and
+            (-not $valueParam.ValueFromPipeline) -and
+            (-not $valueParam.ValueFromPipelineByPropertyName) -and
+            (-not $valueParam.ValueFromRemainingArguments) -and
+            (0 -eq $valueParam.Position) -and
+            (0 -eq $valueParam.Aliases.Count)
+
+        if (-not $pass) {
+            throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+        }
     }
-
-    $paramSets = @($test.Data.out[0].ParameterSets)
-    if (1 -ne $paramSets.Count) {
-        $message = 'Assert-NotTrue failed parameter tests (more than one parameter set).'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
-    }
-
-    $valueParam = $paramSets[0].Parameters |
-        Where-Object {'Value'.Equals($_.Name, [System.StringComparison]::OrdinalIgnoreCase)}
-    if (Test-Null $valueParam) {
-        $message = 'Assert-NotTrue failed parameter tests (no parameter with name "Value").'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
-    }
-
-    $test.Pass =
-        ($valueParam.IsMandatory) -and
-        ($valueParam.ParameterType -eq [System.Object]) -and
-        (-not $valueParam.ValueFromPipeline) -and
-        (-not $valueParam.ValueFromPipelineByPropertyName) -and
-        (-not $valueParam.ValueFromRemainingArguments) -and
-        (0 -eq $valueParam.Position) -and
-        (0 -eq $valueParam.Aliases.Count)
-
-    if (-not $test.Pass) {
-        $message = 'Assert-NotTrue failed parameter tests ("Value" parameter attributes).'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
-    }
+    finally {commitTestLogEntry $test $pass}
 }
 
 & {
-    $test = newTestLogEntry 'Test Assert-NotTrue with Boolean $true'
+    $testDescription = 'Assert-NotTrue with Boolean $true'
 
-    $test.Data.in  = @{value = $true}
-    $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
-    $test.Data.out = $out
+    $test = newTestLogEntry $testDescription
+    $pass = $false
+    try {
+        $test.Data.out = $out = @()
+        $test.Data.in  = @{value = $true}
+        $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+        $test.Data.out = $out
 
-    $test.Pass =
-        ($test.Data.err -is [System.Management.Automation.ErrorRecord]) -and
-        ($test.Data.err.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-NotTrue', [System.StringComparison]::OrdinalIgnoreCase)) -and
-        ($test.Data.out.Count -eq 0)
+        $pass =
+            ($test.Data.err -is [System.Management.Automation.ErrorRecord]) -and
+            ($test.Data.err.FullyQualifiedErrorId.Equals('AssertionFailed,Assert-NotTrue', [System.StringComparison]::OrdinalIgnoreCase)) -and
+            ($test.Data.out.Count -eq 0)
 
-    if (-not $test.Pass) {
-        $message = 'Assert-NotTrue failed with $true value.'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
+        if (-not $pass) {
+            throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+        }
     }
+    finally {commitTestLogEntry $test $pass}
 }
 
 & {
-    $test = newTestLogEntry 'Test Assert-NotTrue with Boolean $false'
+    $testDescription = 'Assert-NotTrue with Boolean $false'
 
-    $test.Data.in  = @{value = $false}
-    $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
-    $test.Data.out = $out
+    $test = newTestLogEntry $testDescription
+    $pass = $false
+    try {
+        $test.Data.out = $out = @()
+        $test.Data.in  = @{value = $false}
+        $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+        $test.Data.out = $out
 
-    $test.Pass =
-        (Test-Null $test.Data.err) -and
-        ($test.Data.out.Count -eq 0)
+        $pass =
+            (Test-Null $test.Data.err) -and
+            ($test.Data.out.Count -eq 0)
 
-    if (-not $test.Pass) {
-        $message = 'Assert-NotTrue failed with $false value.'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
+        if (-not $pass) {
+            throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+        }
     }
+    finally {commitTestLogEntry $test $pass}
 }
 
 & {
-    $test = newTestLogEntry 'Test Assert-NotTrue with $null'
+    $testDescription = 'Assert-NotTrue with $null'
 
-    $test.Data.in  = @{value = $null}
-    $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
-    $test.Data.out = $out
+    $test = newTestLogEntry $testDescription
+    $pass = $false
+    try {
+        $test.Data.out = $out = @()
+        $test.Data.in  = @{value = $null}
+        $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+        $test.Data.out = $out
 
-    $test.Pass =
-        (Test-Null $test.Data.err) -and
-        ($test.Data.out.Count -eq 0)
+        $pass =
+            (Test-Null $test.Data.err) -and
+            ($test.Data.out.Count -eq 0)
 
-    if (-not $test.Pass) {
-        $message = 'Assert-NotTrue failed with $null value.'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
+        if (-not $pass) {
+            throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+        }
     }
+    finally {commitTestLogEntry $test $pass}
 }
 
 & {
+    $testDescription = 'Assert-NotTrue with Non-Boolean that is convertible to $true'
+
     for ($i = 0; $i -lt $nonBooleanTrue.Count; $i++) {
-        $test = newTestLogEntry 'Test Assert-NotTrue with Non-Boolean that is convertible to $true'
+        $test = newTestLogEntry $testDescription
+        $pass = $false
+        try {
+            $test.Data.out = $out = @()
+            $test.Data.in  = @{value = $nonBooleanTrue[$i]}
+            $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+            $test.Data.out = $out
 
-        $test.Data.in  = @{value = $nonBooleanTrue[$i]}
-        $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
-        $test.Data.out = $out
+            $pass =
+                (Test-Null $test.Data.err) -and
+                ($test.Data.out.Count -eq 0)
 
-        $test.Pass =
-            (Test-Null $test.Data.err) -and
-            ($test.Data.out.Count -eq 0)
-
-        if (-not $test.Pass) {
-            $message = 'Assert-NotTrue failed with Non-Boolean that is convertible to $true.'
-            throw New-Object 'System.Exception' -ArgumentList @($message)
+            if (-not $pass) {
+                throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+            }
         }
+        finally {commitTestLogEntry $test $pass}
     }
 
     if ($i -eq 0) {
-        $test = newTestLogEntry 'Test Assert-NotTrue with Non-Boolean that is convertible to $true'
-
-        $message = 'No data to test Assert-NotTrue with Non-Boolean that is convertible to $true.'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
+        commitTestLogEntry (newTestLogEntry $testDescription)
+        throw New-Object 'System.Exception' -ArgumentList @("No data for $testDescription")
     }
 }
 
 & {
+    $testDescription = 'Assert-NotTrue with Non-Boolean that is convertible to $false'
+
     for ($i = 0; $i -lt $nonBooleanFalse.Count; $i++) {
-        $test = newTestLogEntry 'Test Assert-NotTrue with Non-Boolean that is convertible to $false'
+        $test = newTestLogEntry $testDescription
+        $pass = $false
+        try {
+            $test.Data.out = $out = @()
+            $test.Data.in  = @{value = $nonBooleanFalse[$i]}
+            $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
+            $test.Data.out = $out
 
-        $test.Data.in  = @{value = $nonBooleanFalse[$i]}
-        $test.Data.err = try {Assert-NotTrue $test.Data.in.value -OutVariable out | Out-Null} catch {$_}
-        $test.Data.out = $out
+            $pass =
+                (Test-Null $test.Data.err) -and
+                ($test.Data.out.Count -eq 0)
 
-        $test.Pass =
-            (Test-Null $test.Data.err) -and
-            ($test.Data.out.Count -eq 0)
-
-        if (-not $test.Pass) {
-            $message = 'Assert-NotTrue failed with Non-Boolean that is convertible to $false.'
-            throw New-Object 'System.Exception' -ArgumentList @($message)
+            if (-not $pass) {
+                throw New-Object 'System.Exception' -ArgumentList @("Failed $testDescription")
+            }
         }
+        finally {commitTestLogEntry $test $pass}
     }
 
     if ($i -eq 0) {
-        $test = newTestLogEntry 'Test Assert-NotTrue with Non-Boolean that is convertible to $false'
-
-        $message = 'No data to test Assert-NotTrue with Non-Boolean that is convertible to $false.'
-        throw New-Object 'System.Exception' -ArgumentList @($message)
+        commitTestLogEntry (newTestLogEntry $testDescription)
+        throw New-Object 'System.Exception' -ArgumentList @("No data for $testDescription")
     }
 }
