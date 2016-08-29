ee2a74079a339bc51b4b394bd5c43fd04bea9e52
Francis de la Cerna <imfrancisd@users.noreply.github.com>
2016-08-29 01:04:20 -0500

    Initial code for Test-Exists -Quantity.

--- src/LogicFunctions/Test-Exists.ps1
+++ src/LogicFunctions/Test-Exists.ps1
@@ -10,7 +10,12 @@ function Test-Exists
 
         [Parameter(Mandatory = $true, ValueFromPipeline = $false, Position = 1)]
         [System.Management.Automation.ScriptBlock]
-        $Predicate
+        $Predicate,
+
+        [Parameter(Mandatory = $false, ValueFromPipeline = $false, Position = 2)]
+        [ValidateSet('Any', 'Single', 'Multiple')]
+        [System.String]
+        $Quantity = 'Any'
     )
 
     #Do not use the return keyword to return the value
@@ -19,6 +24,8 @@ function Test-Exists
     $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 
     if ($Collection -is [System.Collections.ICollection]) {
+        $exists = $false
+        $found = 0
         $enumerator = & $_7ddd17460d1743b2b6e683ef649e01b7_getEnumerator $Collection
 
         foreach ($item in $enumerator) {
@@ -27,11 +34,26 @@ function Test-Exists
             catch {$PSCmdlet.ThrowTerminatingError((& $_7ddd17460d1743b2b6e683ef649e01b7_newPredicateFailedError -errorRecord $_ -predicate $Predicate))}
 
             if (($result -is [System.Boolean]) -and $result) {
-                $true
-                return
+                $found++
+                if ($Quantity -eq 'Any') {
+                    $exists = $true
+                    break
+                }
+                if ($Quantity -eq 'Single') {
+                    if ($found -gt 1) {
+                        $exists = $false
+                        break
+                    }
+                }
+                if ($Quantity -eq 'Multiple') {
+                    if ($found -gt 1) {
+                        $exists = $true
+                        break
+                    }
+                }
             }
         }
-        $false
+        $exists -or (($found -eq 1) -and ($Quantity -eq 'Single'))
         return
     }
 
